eb60e26a11e3b6156fb0c8b07209f86fb50b4a9e
Dion Bosschieter <dbosschieter@transip.nl>
2020-02-19 08:44:30 +0100

    Get higgs data and split into labels and data

--- src/overfit.py
+++ src/overfit.py
@@ -9,4 +9,33 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 
-print(tf.__version__)
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modeling
+import tensorflow_docs.plots
+
+from IPython import display
+from matplotlib import pyplot
+import numpy
+import pathlib
+import shutil
+import tempfile
+
+logdir = pathlib.Path(tempfile.mkdtemp())/"tensorboard_logs"
+shutil.rmtree(logdir, ignore_errors=True)
+
+gzipped_data = tf.keras.utils.get_file('HIGGS.csv.gz', 'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz')
+
+FEATURES = 28
+
+data = tf.data.experimental.CsvDataset(gzipped_data, [float(),]*(FEATURES+1), compression_type='GZIP')
+
+def pack_row(*row):
+    label = row[0]
+    features = tf.stack(row[1:], 1)
+    return features, label
+
+packed_data = data.batch(10000).map(pack_row).unbatch()
+
+for features, label in packed_data.batch(1000).take(1):
+    print(features[0])
+    pyplot.hist(features.numpy().flatten(), bins = 101)
